b44fd0a91ddbeb412fcc163cf7f0feba407c3699
amrina rossada <amrina0612@gmail.com>
2024-07-07 01:29:37 +0800

    update

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -18,7 +18,7 @@ const Experience = () => {
         e.preventDefault();
 
         console.log('Sending email...');
-        emailjs.sendForm('service_ru43hhk', 'template_zgi3l4y', form.current, '0ilaJGuRSjQ5H4Qs1')
+        emailjs.sendForm('service_ru43hhk','template_zgi3l4y', form.current, '0ilaJGuRSjQ5H4Qs1')
             .then((result) => {
                 console.log(result.text);
                 e.target.reset();
